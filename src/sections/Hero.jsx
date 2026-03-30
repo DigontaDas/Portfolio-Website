b808ac84d181d7adbc7b57537a123a4f82d003f9
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2026-03-30 22:55:28 +0600

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
                     Hi, I am Digonta <span className="waving-hand">👋🏼</span></p>
-                <p className="hero_tag text-gray_gradient">AI researcher and Web Developer</p>
+                <p className="hero_tag text-gray_gradient">AI and MERN Stack Engineer</p>
             </div>
             <div className="w-full h-full absolute inset-0">
                 <Canvas className="w-full h-full">
